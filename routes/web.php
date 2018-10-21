38046e0274b73d2935cd9af86b01d1ddad2c4d40
3A417141 <as05202005@gmail.com>
2018-10-21 23:30:35 +0800

    Sixth update files

--- routes/web.php
+++ routes/web.php
@@ -17,10 +17,11 @@ Route::get('/', function () {
 // 增加新的任務
 Route::post('/task', function (Request $request) {
     // 驗證輸入
-    $validator = Validator::make($request->all(), [
-        'name' => 'required|max:255',
+    $validator = Validator::make( $request->all(), [
+        'name' => 'required|max:255',      //驗證規則
     ]);
-    if ($validator->fails()) {
+
+    if ($validator->fails()) {      //驗證失敗處理
         return redirect('/')
             ->withInput()
             ->withErrors($validator);
@@ -30,6 +31,8 @@ Route::post('/task', function (Request $request) {
     $task->name = $request->name;
     $task->save();
     return redirect('/');
+
+
 
 
 });
